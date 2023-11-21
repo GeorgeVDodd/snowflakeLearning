f06af1cad15377aa0bdc1544bc4c256b60093e91
GeorgeVDodd <150877904+GeorgeVDodd@users.noreply.github.com>
2023-11-20 20:13:33 -0600

    Update gvd_streamlit.py

--- gvd_streamlit.py
+++ gvd_streamlit.py
@@ -1,7 +1,7 @@
 import streamlit
 import pandas 
-import snowflake.connector 
 import requests
+import snowflake.connector 
 from urllib.error import URLError
 
 #Top Layout 
@@ -30,9 +30,9 @@ def get_fruityvice_data(this_fruit_choice):
   return fruitvice_normalized
   
 streamlit.header("Fruityvice Fruit Advice!")
-fruit_choice = streamlit.text_input('What fruit would you like information about?')
+this_fruit_choice = streamlit.text_input('What fruit would you like information about?')
 # removedstreamlit.write('The user entered ', fruit_choice)
-if not fruit_choice: 
+if not this_fruit_choice: 
   streamlit.error("please select a fruit to get more information.") 
 else: 
   back_from_function = get_fruityvice_data(fruit_choice)
@@ -44,7 +44,7 @@ streamlit.stop()
 # allow the user to add a fruit to the list 
 def insert_row_snowflake(new_fruit)
   with my_cur = my_cnx.cursor() as my_cur 
-  my_cur.execute("Insert into the fruit_load_list value ('" + ???? + "')")
+  my_cur.execute("Insert into the fruit_load_list value ('from streamlit)")
   return "Thanks for adding" + new_fruit 
 
 add_my_fruit = streamlit.text_input('What fruit would you like to add?')
